6044f2690ff1ad1628321150a16d1c564a3b578b
thabir303 <109710837+thabir303@users.noreply.github.com>
2023-02-28 22:26:16 -0800

    Add files via upload

---
+++ num_to_string.cpp
@@ -0,0 +1,49 @@
+#include "header.h"
+
+string n_to_s(int n)
+{
+    string num_string="";
+    if(n>9)
+    {
+        int new_num1=n/10;
+        num_string+=(48 + new_num1);
+
+        int new_num2 = n -(new_num1) * 10;
+        num_string += (48 + new_num2);
+    }
+    else {
+        num_string += (48 + n);
+    }
+
+    return num_string;
+}
+int char_type(char c)
+{
+
+    if(an_alphabte(c))
+        return 1;
+    else if(a_digit(c))
+        return 2;
+    else if(an_operator(c))
+        return 3;
+    else if(a_power(c))
+        return 4;
+    else if(a_bracket(c))
+        return 5;
+    else
+        return 0;
+}
+char* c_str(string s)
+{
+    char c[100];
+
+    for(int i=0; i<100; i++)
+    {
+        c[i] = s[i];
+    }
+    c[s.size()] = '\0';
+    for(int i=0; i<strlen(c); i++)
+        cout<<c[i];
+    cout<<"\n";
+    return c;
+}
