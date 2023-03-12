75478f0f09f782e7a2aa177682db758102d43f55
thabir303 <109710837+thabir303@users.noreply.github.com>
2023-03-12 21:13:45 +0600

    Add files via upload

--- header.h
+++ header.h
@@ -1,6 +1,25 @@
 #include<iostream>
+#include <string>
+#include<cstring>
 using namespace std;
 
 void display();
 string for_partition(string equ);
-string Partition(string equ);
+//string Partition(string equ);
+string derivative(char,int,int,char);
+string n_to_s(int n);
+bool ln_function(string equ);
+string if_ln(string equ);
+bool trigonometry_function(string equ);
+int trigono_sign();
+string if_trigonometry(string equ);
+int char_type(char c);
+void all_trigon();
+char* c_str(string);
+
+
+bool an_alphabte(char  s);
+bool a_digit(char  s);
+bool an_operator(char  s);
+bool a_power(char  s);
+bool a_bracket(char  s);
